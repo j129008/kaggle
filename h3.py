628c07c7f27dca5d81a71d7513a511e6d08657b0
j129008 <j129008@gmail.com>
2015-12-06 20:43:20 +0800

    pca version

--- h3.py
+++ h3.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import RandomizedPCA
 
 f = open('./train.csv','r')
 X = []
@@ -16,6 +17,10 @@ f.close()
 X = np.array(X)
 y = np.array(y)
 
+pca = RandomizedPCA(n_components=9)
+pca.fit(X)
+X = pca.fit_transform(X)
+
 clf = RandomForestClassifier(n_estimators=10)
 clf = clf.fit(X, y)
 
@@ -26,10 +31,13 @@ for row in csv.reader(t):
     tX.append(map(int, row)[1:])
 t.close()
 
+tX = pca.transform(tX)
+
 tX = np.array(tX)
 ans = clf.predict(tX)
 
 out = open('ans2.csv','w')
+out.write("Id,Action\n")
 linum = 0
 for ele in ans:
     linum += 1
